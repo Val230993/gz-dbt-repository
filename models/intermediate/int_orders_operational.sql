9b26f1add985cdda962e8f202d0a9a6a82a55b72
Valeria Salsano <v.salsano7@gmail.com>
2025-05-06 20:55:39 +0000

    commit x 3

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -8,6 +8,8 @@ s.ship_cost,
 s.logcost,
 s.orders_id,
 m.purchase_cost,
+m.revenue,
+m.quantity,
 (m.margin + s.shipping_fee - s.ship_cost - s.logcost) AS operational_margin
 FROM {{ ref("stg_gz_raw_data__ship")}} s
 LEFT JOIN {{ ref("int_sales_margin")}} m
