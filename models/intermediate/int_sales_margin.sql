cf84817d8968b4ad749b0e0b94f22dfb1c705965
Valeria Salsano <v.salsano7@gmail.com>
2025-05-07 14:04:03 +0000

    commit margin &

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -5,11 +5,9 @@ SELECT
     s.revenue,
     s.orders_id,
     s.quantity,
-    (p.purchase_price*s.quantity) AS purchase_cost,
-    (s.revenue-(p.purchase_price*s.quantity)) AS margin
-FROM {{ ref("stg_gz_raw_data__product")}} p
-LEFT JOIN  {{ ref("stg_gz_raw_data__sales")}} s
-ON p.products_id = s.product_id
-
-
-
+    (p.purchase_price * s.quantity) AS purchase_cost,
+    s.revenue - p.purchase_price * s.quantity AS margin,
+    {{ margin_percent('s.revenue - p.purchase_price * s.quantity', 's.revenue') }} AS margin_percent
+FROM {{ ref("stg_gz_raw_data__product") }} p
+LEFT JOIN {{ ref("stg_gz_raw_data__sales") }} s
+    ON p.products_id = s.product_id
